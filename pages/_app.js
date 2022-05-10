b3100219b1867c9f90a17dcc2ed8d4eb29b9ab1e
MM Lap <mmdelcampo@aper.com>
2022-05-09 20:59:12 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useRouter, Router} from 'next/router';
 import { Toaster } from 'react-hot-toast';
+
 
 import { Layout } from '../components';
 import '../styles/globals.css';
