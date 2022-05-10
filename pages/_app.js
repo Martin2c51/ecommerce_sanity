d0491b2cd5de2cc462b22e061261d2eb3c5d0fd3
MM Lap <mmdelcampo@aper.com>
2022-05-09 20:54:34 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useRouter, Router} from 'next/router';
 import { Toaster } from 'react-hot-toast';
 
 import { Layout } from '../components';
@@ -6,6 +7,24 @@ import '../styles/globals.css';
 import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+    const pageView = (url, title) => {
+        window && window.dataLayer && window.dataLayer.push({
+            'event': 'virtualPageview',
+            'virtualPageURL': url,
+            'virtualPageTitle': title,
+        });
+    }
+    useEffect(() => {
+        pageView(router.pathname, document.title);
+        const handleRouteChange = (url) => {
+            pageView(url, document.title);
+        };
+        Router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            Router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, []);
   return (
     <StateContext>
       {/* <!-- Google Tag Manager (noscript) --> */}
