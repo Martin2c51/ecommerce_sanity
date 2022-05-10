f854184bf8b4cd698e2dd714837015c945a72804
MM Lap <mmdelcampo@aper.com>
2022-05-09 19:42:42 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,11 @@ import { StateContext } from '../context/StateContext';
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSS5FSL"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
+
       <Layout>
         <Toaster />
         <Component {...pageProps} />
